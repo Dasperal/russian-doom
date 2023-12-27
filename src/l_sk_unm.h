f3dddef1558f3eed54e62b38b2057ceccb4cf7ea
Dasperal <dasperal1@gmail.com>
2023-12-27 22:43:09 +0300

    Hexen: Increase Wraithverge mana usage from 18 to 33 on Skill 6

--- src/l_sk_unm.h
+++ src/l_sk_unm.h
@@ -17,35 +17,51 @@
 #define UNM_DECLARE_ATTR_VARIABLE(class_name, attr, change_expr) \
 static int class_name ## _ ## attr;
 
+#define UNM_DECLARE_PROP_VARIABLE(var_name, property, change_expr) \
+static int var_name;
+
 #define UNM_APPLY_ATTR_CHANGE(class_name, attr, change_expr) \
 mobjinfo[class_name].attr = class_name ## _ ## attr change_expr;
 
 #define UNM_RESTORE_ATTR(class_name, attr, change_expr) \
 mobjinfo[class_name].attr = class_name ## _ ## attr;
 
-#define UNM_IMPLEMENT_APPLY_RESTORE_ATTRS(O)    \
+#define UNM_APPLY_PROP_CHANGE(var_name, property, change_expr) \
+property = var_name change_expr;
+
+#define UNM_RESTORE_PROP(var_name, property, change_expr) \
+property = var_name;
+
+#define UNM_IMPLEMENT_APPLY_RESTORE_ATTRS(O, P) \
 void UNM_Apply_Restore_Atters(skill_t skill)    \
 {                                               \
     if(skill == sk_ultranm)                     \
     {                                           \
         O(UNM_APPLY_ATTR_CHANGE)                \
+        P(UNM_APPLY_PROP_CHANGE)                \
     }                                           \
     else                                        \
     {                                           \
         O(UNM_RESTORE_ATTR)                     \
+        P(UNM_RESTORE_PROP)                     \
     }                                           \
 }
 
 #define UNM_SAVE_ATTR(class_name, attr, change_expr) \
 class_name ## _ ## attr = mobjinfo[class_name].attr;
 
-#define UNM_IMPLEMENT_SAVE_ATTRS(O) \
-void UNM_Save_Atters(void)          \
-{                                   \
-    O(UNM_SAVE_ATTR)                \
+#define UNM_SAVE_PROPERTY(var_name, property, change_expr) \
+var_name = property;
+
+#define UNM_IMPLEMENT_SAVE_ATTRS(O, P) \
+void UNM_Save_Atters(void)             \
+{                                      \
+    O(UNM_SAVE_ATTR)                   \
+    P(UNM_SAVE_PROPERTY)               \
 }
 
-#define UNM_IMPLEMENT(O)                \
+#define UNM_IMPLEMENT(O, P)             \
 O(UNM_DECLARE_ATTR_VARIABLE)            \
-UNM_IMPLEMENT_APPLY_RESTORE_ATTRS(O)    \
-UNM_IMPLEMENT_SAVE_ATTRS(O)
+P(UNM_DECLARE_PROP_VARIABLE)            \
+UNM_IMPLEMENT_APPLY_RESTORE_ATTRS(O, P) \
+UNM_IMPLEMENT_SAVE_ATTRS(O, P)
