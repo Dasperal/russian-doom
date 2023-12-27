c7c7b43dd9a07a8459dd8f654d45498d5b8cb97b
Dasperal <dasperal1@gmail.com>
2023-12-27 21:41:32 +0300

    Heretic: Increase damage for Red Axe, Minotaur fireballs and firewall, D'sparil's Serpent fireballs on Skill 6

--- src/heretic/g_sk_unm.c
+++ src/heretic/g_sk_unm.c
@@ -18,32 +18,36 @@
 
 #include "info.h"
 
-#define ENUMERATE_UNM_CHANGES(O)                                                \
-/* Monster's speed */                                                           \
-O(MT_IMP, speed, + 7)              /* Gargolye (10 to 17) */                    \
-O(MT_IMPLEADER, speed, + 7)        /* Fire Gargolye (10 to 17) */               \
-O(MT_MUMMY, speed, + 9)            /* Golem (12 to 21) */                       \
-O(MT_MUMMYGHOST, speed, + 9)       /* Golem Ghost (12 to 21) */                 \
-O(MT_MUMMYLEADER, speed, + 9)      /* Nitrogolem (12 to 21) */                  \
-O(MT_MUMMYLEADERGHOST, speed, + 9) /* Nitrogolem Ghost (12 to 21) */            \
-O(MT_CLINK, speed, + 14)           /* Sabreclaw (14 to 28) */                   \
-O(MT_KNIGHT, speed, + 7)           /* Undead Warrior (12 to 19) */              \
-O(MT_KNIGHTGHOST, speed, + 7)      /* Undead Warrior Ghost (12 to 19) */        \
-O(MT_BEAST, speed, + 7)            /* Weredragon (14 to 21) */                  \
-O(MT_SNAKE, speed, + 7)            /* Ophidian (10 to 17) */                    \
-O(MT_WIZARD, speed, + 7)           /* Disciple of D'Sparil (12 to 19) */        \
-O(MT_HEAD, speed, + 7)             /* Iron Lich (6 to 13) */                   \
-O(MT_MINOTAUR, speed, + 5)         /* Maolotaur (16 to 21) */                   \
-O(MT_SORCERER1, speed, + 5)        /* D'Sparil (riding) (16 to 21) */           \
-O(MT_SORCERER2, speed, + 6)        /* D'Sparil (walking) (14 to 20) */          \
-/* Monster's damage */                                                          \
-O(MT_IMPBALL, damage, + 1)          /* Fire Gargolye fireball (1 to 2) */        \
-O(MT_MUMMYFX1, damage, + 2)         /* Nitrogolem Ghost fireball (4 to 6) */     \
-O(MT_KNIGHTAXE, damage, + 2)        /* Undead Warrior green axe (3 to 5) */      \
-O(MT_REDAXE, damage, + 1)           /* Undead Warrior red axe (7 to 8) */        \
-O(MT_BEASTBALL, damage, + 1)        /* Weredragon fireball (4 to 5) */           \
-O(MT_SNAKEPRO_A, damage, + 1)       /* Ophidian fireball A (1 to 2) */           \
-O(MT_SNAKEPRO_B, damage, + 1)       /* Ophidian fireball B (3 to 4) */           \
-O(MT_WIZFX1, damage, + 1)           /* Disciple of D'Sparil fireball (3 to 4) */
+#define ENUMERATE_UNM_CHANGES(O)                                                 \
+/* Monster's speed */                                                            \
+O(MT_IMP, speed, + 7)              /* Gargolye (10 to 17) */                     \
+O(MT_IMPLEADER, speed, + 7)        /* Fire Gargolye (10 to 17) */                \
+O(MT_MUMMY, speed, + 9)            /* Golem (12 to 21) */                        \
+O(MT_MUMMYGHOST, speed, + 9)       /* Golem Ghost (12 to 21) */                  \
+O(MT_MUMMYLEADER, speed, + 9)      /* Nitrogolem (12 to 21) */                   \
+O(MT_MUMMYLEADERGHOST, speed, + 9) /* Nitrogolem Ghost (12 to 21) */             \
+O(MT_CLINK, speed, + 14)           /* Sabreclaw (14 to 28) */                    \
+O(MT_KNIGHT, speed, + 7)           /* Undead Warrior (12 to 19) */               \
+O(MT_KNIGHTGHOST, speed, + 7)      /* Undead Warrior Ghost (12 to 19) */         \
+O(MT_BEAST, speed, + 7)            /* Weredragon (14 to 21) */                   \
+O(MT_SNAKE, speed, + 7)            /* Ophidian (10 to 17) */                     \
+O(MT_WIZARD, speed, + 7)           /* Disciple of D'Sparil (12 to 19) */         \
+O(MT_HEAD, speed, + 7)             /* Iron Lich (6 to 13) */                     \
+O(MT_MINOTAUR, speed, + 5)         /* Maolotaur (16 to 21) */                    \
+O(MT_SORCERER1, speed, + 5)        /* D'Sparil (riding) (16 to 21) */            \
+O(MT_SORCERER2, speed, + 6)        /* D'Sparil (walking) (14 to 20) */           \
+/* Monster's damage */                                                           \
+O(MT_IMPBALL, damage, + 1)         /* Fire Gargolye fireball (1 to 2) */         \
+O(MT_MUMMYFX1, damage, + 2)        /* Nitrogolem Ghost fireball (4 to 6) */      \
+O(MT_KNIGHTAXE, damage, + 2)       /* Undead Warrior green axe (3 to 5) */       \
+O(MT_REDAXE, damage, + 2)          /* Undead Warrior red axe (7 to 9) */         \
+O(MT_BEASTBALL, damage, + 1)       /* Weredragon fireball (4 to 5) */            \
+O(MT_SNAKEPRO_A, damage, + 1)      /* Ophidian fireball A (1 to 2) */            \
+O(MT_SNAKEPRO_B, damage, + 1)      /* Ophidian fireball B (3 to 4) */            \
+O(MT_WIZFX1, damage, + 1)          /* Disciple of D'Sparil fireball (3 to 4) */  \
+O(MT_MNTRFX1, damage, + 1)         /* Minotaur 5 fireballs (3 to 4) */           \
+O(MT_MNTRFX2, damage, + 2)         /* Minotaur firewall 1 (4 to 6) */            \
+O(MT_MNTRFX3, damage, + 2)         /* Minotaur firewall 2 (4 to 6) */            \
+O(MT_SRCRFX1, damage, + 2)         /* D'sparil's Serpent firewall (10 to 12) */
 
 UNM_IMPLEMENT(ENUMERATE_UNM_CHANGES)
