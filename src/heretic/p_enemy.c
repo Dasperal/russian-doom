8a8906c5bbc25d68eedce87cc4e929bbbd27a6e8
Dasperal <dasperal1@gmail.com>
2024-03-21 23:51:29 +0300

    Heretic: Change minotaur's floor fire attack chance on Skill 6

--- src/heretic/p_enemy.c
+++ src/heretic/p_enemy.c
@@ -1701,8 +1701,9 @@ void A_MinotaurDecide(mobj_t *actor, player_t *player, pspdef_t *psp)
         actor->momy = FixedMul(MNTR_CHARGE_SPEED, finesine[angle]);
         actor->special1.i = 35 / 2;       // Charge duration
     }
-    else if (target->z == target->floorz
-             && dist < 9 * 64 * FRACUNIT && P_Random() < 220)
+    else if ((target->z == target->floorz
+              && dist < 9 * 64 * FRACUNIT && P_Random() < 220)
+         || (gameskill == sk_ultranm && P_Random() > 200))
     {                           // Floor fire attack
         P_SetMobjState(actor, S_MNTR_ATK3_1);
         actor->special2.i = 0;
